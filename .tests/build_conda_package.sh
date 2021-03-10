fdcb809d4394fc5d4db1b3a28d2b1ca38431dcde
SGMartin <s.g.martin94@gmail.com>
2021-03-10 09:59:26 +0100

    Update recipe path to new repo

--- .tests/build_conda_package.sh
+++ .tests/build_conda_package.sh
@@ -6,6 +6,6 @@ else
     PYTHON_VERSION=$1
 fi
 
-git clone https://gitlab.com/bu_cnio/beyondcell_conda_recipe
-conda mambabuild beyondcell_conda_recipe/r-beyondcell --output-folder ./
+git clone https://gitlab.com/bu_cnio/conda-recipes
+conda mambabuild conda-recipes/r-beyondcell --output-folder ./
 mamba install --use-local --update-deps linux-64/r-beyondcell-*.tar.bz2
