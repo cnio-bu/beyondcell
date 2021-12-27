c9f93b616a92392128b6211235746c3ffc0a09fa
mjjimenez <mj.jimenezsantos@outlook.com>
2021-12-27 16:59:57 +0100

    Add "assays" to beyondcell slot "SeuratInfo"

--- R/Score.R
+++ R/Score.R
@@ -55,8 +55,8 @@ bcScore <- function(sc, gs, expr.thres = 0.1) {
   # --- Code ---
   # Create beyondcell object.
   bc <- beyondcell(expr.matrix = expr.matrix, meta.data = sc@meta.data,
-                   SeuratInfo = list(reductions = sc@reductions, 
-                                     images = sc@images),
+                   SeuratInfo = list(assays = default, images = sc@images,
+                                     reductions = sc@reductions),
                    regression = list(order = rep("", 2), vars = NULL,
                                      order.background = rep("", 2)),
                    n.genes = gs@n.genes, mode = gs@mode, thres = expr.thres)
