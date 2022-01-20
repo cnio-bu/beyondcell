8e161d6ef1d785ebfe711b92b3f3851277471f3e
Maria Jose Jimenez-Santos <mj.jimenezsantos@outlook.com>
2022-01-20 15:44:22 +0100

    Avoid warning message in RunPCA

--- R/Reductions.R
+++ R/Reductions.R
@@ -178,7 +178,8 @@ bcUMAP <- function(bc, pc = NULL, k.neighbors = 20, res = 0.2,
   n.dims <- min(50, n.drugs)
   sc <- Seurat::ScaleData(sc, features = rownames(sc), do.scale = FALSE,
                           do.center = FALSE)
-  sc <- Seurat::RunPCA(sc, features = rownames(sc), npcs = 100, maxit = 100000)
+  sc <- Seurat::RunPCA(sc, features = rownames(sc), npcs = 100, maxit = 100000,
+                       nfeatures.print = min(30, n.drugs/2))
   # Elbow plot.
   elbowplot <- Seurat::ElbowPlot(sc, ndims = n.dims - 1) +
     ggplot2::theme(legend.position = "bottom")
