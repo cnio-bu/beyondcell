a1ccf0ab40c265f643222c37a0e659c471c9cdfb
Maria Jose Jimenez-Santos <mj.jimenezsantos@outlook.com>
2022-10-31 14:24:23 +0100

    Add warning tests for bcScore

--- tests/testthat/test-bcScore.R
+++ tests/testthat/test-bcScore.R
@@ -1,3 +1,12 @@
+# --- Functions ---
+# Function to capitalize genes (mouse symbol-like).
+capitalize <- function(x) {
+  x <- tolower(x)
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  return(x)
+}
+
+# --- Code ---
 # PBMC data.
 pbmc.data <- Seurat::Read10X("../testdata/")
 
@@ -5,7 +14,7 @@ pbmc.data <- Seurat::Read10X("../testdata/")
 pbmc.raw <- Seurat::CreateSeuratObject(counts = pbmc.data, project = "pbmc3k",
                                        min.cells = 3, min.features = 20)
 
-# Normalize
+# Normalize.
 pbmc <- Seurat::NormalizeData(pbmc.raw, normalization.method = "LogNormalize",
                               scale.factor = 10000)
 
@@ -14,8 +23,43 @@ pbmc.fake <- pbmc
 pbmc.fake[["ADT"]] <- Seurat::CreateAssayObject(counts = pbmc.data)
 Seurat::DefaultAssay(pbmc.fake) <- "ADT"
 
+# Single-cell expression matrix.
+mtx <- as.matrix(pbmc@assays$RNA@data)
+
 # Geneset object.
 ssc <- GetCollection(SSc, n.genes = 100)
+
+# Geneset with "mouse" genes.
+ssc.mouse <- ssc
+ssc.mouse@genelist <- lapply(ssc.mouse@genelist, FUN = function(x) {
+  lapply(x, function(y) capitalize(y))})
+ssc.mouse@info <- data.frame()
+
+# Small experiment with low expression cells for 2/3 of signatures.
+### Small geneset with 3 signatures (only up).
+ngenes <- 50
+ssc.lowexpr <- GetCollection(SSc, n.genes = ngenes, mode = "up",
+                             filters = list(IDs = c("sig-20879", "sig-20880", 
+                                                    "sig-20881")),
+                             include.pathways = FALSE)
+### Unique genes in all 3 signatures.
+all.genes <- unique(unlist(ssc.lowexpr@genelist))
+### Subset pbmc.data to keep as many rows as unique genes.
+pbmc.data.lowexpr <- pbmc.data[1:length(all.genes), ]
+### Change rownames.
+rownames(pbmc.data.lowexpr) <- all.genes
+### Make all expression values = 0...
+pbmc.data.lowexpr[, ] <- 10
+### Except for >10% of the genes in signatures 2 and 3 (expression = 10).
+pbmc.data.lowexpr[ssc.lowexpr@genelist[[2]][["up"]][1:((0.1*ngenes)+1)], ] <- 10
+pbmc.data.lowexpr[ssc.lowexpr@genelist[[3]][["up"]][1:((0.1*ngenes)+1)], ] <- 10
+### Create Seurat object.
+pbmc.raw.lowexpr <- Seurat::CreateSeuratObject(counts = pbmc.data.lowexpr, 
+                                               project = "lowexpr",
+                                               min.cells = 3, min.features = 2)
+### And normalize.
+pbmc.lowexpr <- Seurat::NormalizeData(pbmc.raw.lowexpr, scale.factor = 10000, 
+                                      normalization.method = "LogNormalize")
 
 # Test errors.
 testthat::test_that("errors", {
@@ -55,5 +99,33 @@ testthat::test_that("errors", {
   testthat::expect_error(
     bcScore(pbmc, gs = ssc, expr.thres = -2),
     'expr.thres must be a positive number between 0 and 1.'
+  )
+})
+
+# Test warnings.
+testthat::test_that("warnings", {
+  ### Check when sc is an expression matrix.
+  testthat::expect_equal(
+    testthat::capture_warning(
+      bcScore(mtx, gs = ssc)
+    )$message,
+    paste('Using count matrix as input. Please, check that this matrix',
+          'is normalized and unscaled.')
+  )
+  ### Check when gene names are not in the same format.
+  testthat::expect_equal(
+    testthat::capture_warning(
+      bcScore(pbmc, gs = ssc.mouse)
+    )$message,
+    paste('gs genes are capitalized and sc genes are in uppercase. Please', 
+          'check your Seurat object and translate the genes if necessary.')
+  )
+  ### Check signatures for which no cells pass the expr.thres.
+  testthat::expect_equal(
+    testthat::capture_warning(
+      bcScore(pbmc.lowexpr, gs = ssc.lowexpr)
+    )$message,
+    paste('The following signatures have no cells that pass the expr.thres and',
+          'will be removed: "sig-20880", "sig-20881".')
   )
 })
